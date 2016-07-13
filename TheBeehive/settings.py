be552af1dd2b15c3b9e852219e5f256a25fab561
YannSerrand <yann.serrand@gmail.com>
2016-07-13 07:53:51 +0200

    Add files via upload
    Changes in settings :
    - add database settings,
    - add app in INSTALLED_APPS

--- TheBeehive/settings.py
+++ TheBeehive/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'app.apps.AppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -76,10 +77,14 @@ WSGI_APPLICATION = 'TheBeehive.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'TheBeehiveDB',                      # Or path to database file if using sqlite3.
+        'USER': 'queen',                      # Not used with sqlite3.
+        'PASSWORD': 'shesthequeen',                  # Not used with sqlite3.
+        'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '5432',                      # Set to empty string for default. Not used with sqlite3.
+    }}
+
 
 
 # Password validation
