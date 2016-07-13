379f1ec1054513ddeb59f261abddbd2d566ad633
YannSerrand <yann.serrand@gmail.com>
2016-07-13 07:49:13 +0200

    Add files via upload
    - First draw of the models
    - First three views : index, sales and sales detail
    - Correponding urls

--- app/models.py
+++ app/models.py
@@ -3,3 +3,73 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Client(models.Model):
+    
+    first_name = models.CharField(verbose_name = 'First name', max_length = 50)
+    last_name = models.CharField(verbose_name = 'Last name', max_length = 50)
+    address = models.TextField()
+    
+
+class Producer(models.Model):
+    
+    first_name = models.CharField(verbose_name = 'First name', max_length = 50)
+    last_name = models.CharField(verbose_name = 'Last name', max_length = 50)
+    address = models.TextField()
+    description = models.TextField()
+    picture = models.CharField(max_length = 100)
+    organic = models.BooleanField()
+    
+
+class UnitType(models.Model):
+    
+    name = models.CharField(max_length = 50)
+
+
+class ProductType(models.Model):
+    
+    name = models.CharField(max_length = 50)
+    description = models.TextField()
+    
+    
+class Product(models.Model):
+    
+    producer = models.ForeignKey(Producer)
+    name = models.CharField(max_length = 200)
+    description = models.TextField()
+    product_type = models.ForeignKey(ProductType)
+    quantity = models.FloatField()
+    unit = models.ForeignKey(UnitType)
+    price = models.FloatField()
+    picture = models.CharField(max_length = 100)
+
+
+class Sale(models.Model):
+    
+    date = models.DateField()
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+    is_active = models.BooleanField()
+    
+
+class ProductPerSale(models.Model):
+    
+    sale = models.ForeignKey(Sale)
+    product = models.ForeignKey(Product)
+    initial_quantity = models.IntegerField()
+    available_quantity = models.IntegerField()
+
+
+class Cart(models.Model):
+    
+    client = models.ForeignKey(Client)
+    sale = models.ForeignKey(Sale)
+    products = models.ManyToManyField(Product)
+
+class Command(models.Model):
+    
+    client = models.ForeignKey(Client)
+    sale = models.ForeignKey(Sale)
+    products = models.ManyToManyField(Product)
+    
+    
--- app/views.py
+++ app/views.py
@@ -1,7 +1,38 @@
 from django.shortcuts import render
-
+from django.template import RequestContext 
 from django.http import HttpResponse
+
+from models import *
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at The Beehive index. More to come...") 
+    
+    return render(request, "home.html")
+
+
+def sales(request):
+    
+    last_sales = Sale.objects.all().order_by('-date')    
+    
+    #to add        
+    #nb_producers = 
+    
+    return render(request, "sales.html", {'sales': last_sales})
+
+
+def sales_detail(request, sale_id):
+    
+    # to add : test if there is none
+    s = Sale.objects.filter(id__exact = sale_id)[0]
+    
+    producers = ProductPerSale.objects.filter(sale_id__exact = sale_id).values_list('product__producer').distinct()
+    
+    products = []
+    
+    for p in producers:
+        print ProductPerSale.objects.filter(sale_id__exact = sale_id, product__producer__id = p[0])
+        products.append(ProductPerSale.objects.filter(sale_id__exact = sale_id, product__producer__id = p[0]))
+    
+    return render(request, "sales_detail.html", {'sale': s, 'products': products})
+    
+    
