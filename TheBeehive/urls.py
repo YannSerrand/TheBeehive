ce9975c1d4d5c1155260fcfdb5ee2892c2ced364
YannSerrand <yann.serrand@gmail.com>
2016-07-14 23:44:35 +0200

    Change urls
    Changes to meet django 110 standards (string view argument to url() function)

--- TheBeehive/urls.py
+++ TheBeehive/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.static import serve as static_serve
 import settings
 
 urlpatterns = [
@@ -23,7 +24,7 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     
     
-    url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {
+    url(r'^static/(?P<path>.*)$', static_serve, {
             'document_root': settings.STATIC_ROOT,
         }),
 
